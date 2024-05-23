c72a11bb8f660b0413b99e59eb8a6db90d442849
cyborg97 <muhametshin.1997@gmail.com>
2024-05-23 19:14:47 +0200

    fix: error handling

--- src/entryPoints/bot.ts
+++ src/entryPoints/bot.ts
@@ -7,6 +7,7 @@ import { getTONTokenId, getTicker } from "../utils/coinmarketcap";
 import { getFiatCurrency } from "../utils/currency";
 import { waitTONRPSDelay } from "../utils/runtime";
 import logger from "../utils/logger";
+import { inspect } from "util";
 
 export async function initBot() {
   const httpClient = new HttpClient({
@@ -27,194 +28,207 @@ export async function initBot() {
 
   telegraf.telegram.setMyCommands([{ command: "start", description: "Start" }]);
   telegraf.on("message", async (ctx) => {
-    const [config, created] = await configDao.findOrCreateConfig(
-      ctx.chat.id.toString(),
-    );
-
-    if (ctx.chat.type !== "private") {
-      const chatMember = await ctx.telegram.getChatMember(
-        ctx.chat.id,
-        ctx.message.from.id,
+    try {
+      const [config, created] = await configDao.findOrCreateConfig(
+        ctx.chat.id.toString(),
       );
-      if (
-        !chatMember ||
-        (chatMember.status !== "administrator" &&
-          chatMember.status !== "creator")
-      ) {
-        return;
-      }
-    }
 
-    // @ts-expect-error message.text is string | undefined
-    if (config && !created && ctx.message.text?.startsWith("/start")) {
-      await configDao.deleteForChat(ctx.chat.id.toString());
-      await ctx.reply("Configuration deleted, I won't disturb you anymore");
-      return;
-    }
-
-    if (!config.value.tokenRequested) {
-      await ctx.reply("Please send me an address of token to track");
-      config.value.tokenRequested = true;
-      await configDao.updateConfig(config);
-      return;
-    }
-    if (config.value.tokenRequested && config.tokenAddress === null) {
-      // @ts-expect-error message.text is string | undefined
-      const tokenAddress = ctx.message?.text;
-      if (
-        !Address.isAddress(tokenAddress) &&
-        !Address.isFriendly(tokenAddress) &&
-        !Address.isRaw(tokenAddress)
-      ) {
-        await ctx.reply("Please send me a valid address of token to track");
-        return;
-      }
-      try {
-        const tokenData = await client.jettons.getJettonInfo(tokenAddress);
-        await waitTONRPSDelay();
-
-        if (tokenData.holders_count === 0) {
-          await ctx.reply(`There are no holders for token ${tokenAddress}`);
-          return;
-        }
-        config.tokenAddress = tokenAddress;
-        await configDao.updateConfig(config);
-
-        const dbTicker = await tickerDao.getOrCreateTicker(tokenAddress);
-        if (dbTicker.conmarketcapId === null) {
-          dbTicker.conmarketcapId = await getTONTokenId(tokenAddress);
-          if (dbTicker.conmarketcapId !== null) {
-            await tickerDao.updateTicker(dbTicker);
-          }
-        }
-        if (dbTicker.conmarketcapId === null) {
-          await ctx.reply(
-            `Got token ${tokenData.metadata.symbol} with ${tokenData.holders_count} holders. Though I can't find it on coinmarketcap. I'll try to find it later`,
-          );
-          return;
-        }
-        if (dbTicker.value === null) {
-          const newTickerValue = await getTicker(dbTicker.conmarketcapId);
-          dbTicker.value = newTickerValue;
-          if (newTickerValue !== null) {
-            await tickerDao.updateTicker(dbTicker);
-          }
-        }
-        if (dbTicker.value === null) {
-          await ctx.reply(
-            `Got token ${tokenData.metadata.symbol} with ${tokenData.holders_count} holders. Though there is no ticker for it. I'll try to find it later`,
-          );
-          return;
-        }
-        const fiatCurrency = getFiatCurrency();
-        const fiatFormat = new Intl.NumberFormat("en", {
-          style: "currency",
-          currency: fiatCurrency,
-        });
-
-        await ctx.reply(
-          `Got token ${tokenData.metadata.symbol} with ${
-            tokenData.holders_count
-          } holders\nPrice is ${fiatFormat.format(parseFloat(dbTicker.value))}`,
+      if (ctx.chat.type !== "private") {
+        const chatMember = await ctx.telegram.getChatMember(
+          ctx.chat.id,
+          ctx.message.from.id,
         );
-      } catch (err) {
-        await ctx.reply("Unable to get token info");
-        return;
+        if (
+          !chatMember ||
+          (chatMember.status !== "administrator" &&
+            chatMember.status !== "creator")
+        ) {
+          return;
+        }
       }
-    }
 
-    if (!config.value.emojiRequested) {
-      config.value.emojiRequested = true;
-      await configDao.updateConfig(config);
-      await ctx.reply("Please send me some emoji or 'no' to disable");
-      return;
-    }
-    if (config.value.emojiRequested && config.value.emoji === null) {
       // @ts-expect-error message.text is string | undefined
-      if (ctx.message.text?.toLowerCase() === "no") {
-        config.value.emoji = false;
-        await ctx.reply("Okay, got it");
+      if (config && !created && ctx.message.text?.startsWith("/start")) {
+        await configDao.deleteForChat(ctx.chat.id.toString());
+        await ctx.reply("Configuration deleted, I won't disturb you anymore");
         return;
       }
-      // @ts-expect-error message.text is string | undefined
-      if (ctx.message.text?.length >= 1) {
+
+      if (!config.value.tokenRequested) {
+        await ctx.reply("Please send me an address of token to track");
+        config.value.tokenRequested = true;
+        await configDao.updateConfig(config);
+        return;
+      }
+      if (config.value.tokenRequested && config.tokenAddress === null) {
         // @ts-expect-error message.text is string | undefined
-        config.value.emoji = ctx.message?.text;
-        await ctx.reply("Okay, I'll use this emoji");
-      }
-    }
+        const tokenAddress = ctx.message?.text;
+        if (
+          !Address.isAddress(tokenAddress) &&
+          !Address.isFriendly(tokenAddress) &&
+          !Address.isRaw(tokenAddress)
+        ) {
+          await ctx.reply("Please send me a valid address of token to track");
+          return;
+        }
+        try {
+          const tokenData = await client.jettons.getJettonInfo(tokenAddress);
+          await waitTONRPSDelay();
 
-    if (!config.value.gifRequested) {
-      config.value.gifRequested = true;
-      await configDao.updateConfig(config);
-      await ctx.reply(
-        "Please send me some animation or video or 'no' to disable",
-      );
-      return;
-    }
-    if (
-      config.value.gifRequested &&
-      config.value.gif === null &&
-      (config.value.photo ?? null) === null
-    ) {
-      // @ts-expect-error message.text is string | undefined
-      if (ctx.message.text?.toLowerCase() === "no") {
-        config.value.gif = false;
-        await ctx.reply("Okay, got it");
+          if (tokenData.holders_count === 0) {
+            await ctx.reply(`There are no holders for token ${tokenAddress}`);
+            return;
+          }
+          config.tokenAddress = tokenAddress;
+          await configDao.updateConfig(config);
+
+          const dbTicker = await tickerDao.getOrCreateTicker(tokenAddress);
+          if (dbTicker.conmarketcapId === null) {
+            dbTicker.conmarketcapId = await getTONTokenId(tokenAddress);
+            if (dbTicker.conmarketcapId !== null) {
+              await tickerDao.updateTicker(dbTicker);
+            }
+          }
+          if (dbTicker.conmarketcapId === null) {
+            await ctx.reply(
+              `Got token ${tokenData.metadata.symbol} with ${tokenData.holders_count} holders. Though I can't find it on coinmarketcap. I'll try to find it later`,
+            );
+            return;
+          }
+          if (dbTicker.value === null) {
+            const newTickerValue = await getTicker(dbTicker.conmarketcapId);
+            dbTicker.value = newTickerValue;
+            if (newTickerValue !== null) {
+              await tickerDao.updateTicker(dbTicker);
+            }
+          }
+          if (dbTicker.value === null) {
+            await ctx.reply(
+              `Got token ${tokenData.metadata.symbol} with ${tokenData.holders_count} holders. Though there is no ticker for it. I'll try to find it later`,
+            );
+            return;
+          }
+          const fiatCurrency = getFiatCurrency();
+          const fiatFormat = new Intl.NumberFormat("en", {
+            style: "currency",
+            currency: fiatCurrency,
+          });
+
+          await ctx.reply(
+            `Got token ${tokenData.metadata.symbol} with ${
+              tokenData.holders_count
+            } holders\nPrice is ${fiatFormat.format(
+              parseFloat(dbTicker.value),
+            )}`,
+          );
+        } catch (err) {
+          await ctx.reply("Unable to get token info");
+          return;
+        }
+      }
+
+      if (!config.value.emojiRequested) {
+        config.value.emojiRequested = true;
+        await configDao.updateConfig(config);
+        await ctx.reply("Please send me some emoji or 'no' to disable");
         return;
       }
-      // @ts-expect-error message.animation is expected
-      if (!ctx.message.animation && !ctx.message.video && !ctx.message.photo) {
-        await ctx.reply("Please send me an animation (video or gif)");
-        return;
+      if (config.value.emojiRequested && config.value.emoji === null) {
+        // @ts-expect-error message.text is string | undefined
+        if (ctx.message.text?.toLowerCase() === "no") {
+          config.value.emoji = false;
+          await ctx.reply("Okay, got it");
+          return;
+        }
+        // @ts-expect-error message.text is string | undefined
+        if (ctx.message.text?.length >= 1) {
+          // @ts-expect-error message.text is string | undefined
+          config.value.emoji = ctx.message?.text;
+          await ctx.reply("Okay, I'll use this emoji");
+        }
       }
 
-      // @ts-expect-error message.animation is expected
-      if (ctx.message.animation || ctx.message.video) {
-        config.value.gif =
+      if (!config.value.gifRequested) {
+        config.value.gifRequested = true;
+        await configDao.updateConfig(config);
+        await ctx.reply(
+          "Please send me some animation or video or 'no' to disable",
+        );
+        return;
+      }
+      if (
+        config.value.gifRequested &&
+        config.value.gif === null &&
+        (config.value.photo ?? null) === null
+      ) {
+        // @ts-expect-error message.text is string | undefined
+        if (ctx.message.text?.toLowerCase() === "no") {
+          config.value.gif = false;
+          await ctx.reply("Okay, got it");
+          return;
+        }
+        if (
           // @ts-expect-error message.animation is expected
-          ctx.message.video?.file_id ?? ctx.message.animation?.file_id;
-        // @ts-expect-error message.photo is expected
-        await ctx.replyWithVideo(config.value.gif);
-        // @ts-expect-error message.photo is expected
-      } else if (ctx.message.photo) {
-        config.value.photo =
+          !ctx.message.animation &&
+          // @ts-expect-error message.video is expected
+          !ctx.message.video &&
           // @ts-expect-error message.photo is expected
-          ctx.message.photo[ctx.message.photo.length - 1]?.file_id;
-        await ctx.replyWithPhoto(config.value.photo as string);
-      }
-      await configDao.updateConfig(config);
-      await ctx.reply("Okay, I'll use this animation");
-    }
+          !ctx.message.photo
+        ) {
+          await ctx.reply("Please send me an animation (video or gif)");
+          return;
+        }
 
-    if (!config.value.minBuyRequested) {
-      config.value.minBuyRequested = true;
-      await configDao.updateConfig(config);
-      await ctx.reply(
-        "Please send me the minimum amount of tokens to care about or 'no' to notify about all purchases",
-      );
-      return;
-    }
-    if (config.value.minBuyRequested && config.value.minBuy === null) {
-      // @ts-expect-error message.text is string | undefined
-      if (ctx.message.text?.toLowerCase() === "no") {
-        config.value.minBuy = false;
+        // @ts-expect-error message.animation is expected
+        if (ctx.message.animation || ctx.message.video) {
+          config.value.gif =
+            // @ts-expect-error message.animation is expected
+            ctx.message.video?.file_id ?? ctx.message.animation?.file_id;
+          // @ts-expect-error message.photo is expected
+          await ctx.replyWithVideo(config.value.gif);
+          // @ts-expect-error message.photo is expected
+        } else if (ctx.message.photo) {
+          config.value.photo =
+            // @ts-expect-error message.photo is expected
+            ctx.message.photo[ctx.message.photo.length - 1]?.file_id;
+          await ctx.replyWithPhoto(config.value.photo as string);
+        }
         await configDao.updateConfig(config);
-        await ctx.reply("Okay, got it");
+        await ctx.reply("Okay, I'll use this animation");
+      }
+
+      if (!config.value.minBuyRequested) {
+        config.value.minBuyRequested = true;
+        await configDao.updateConfig(config);
+        await ctx.reply(
+          "Please send me the minimum amount of tokens to care about or 'no' to notify about all purchases",
+        );
         return;
       }
-      // @ts-expect-error message.text is string | undefined
-      if (Number.isNaN(parseInt(ctx.message.text))) {
-        await ctx.reply("Please send me a valid number");
-        return;
-      }
-      // @ts-expect-error message.text is string | undefined
-      if (ctx.message.text?.length >= 1) {
+      if (config.value.minBuyRequested && config.value.minBuy === null) {
         // @ts-expect-error message.text is string | undefined
-        config.value.minBuy = ctx.message.text.toString();
-        await configDao.updateConfig(config);
-        await ctx.reply("Okay, I'll use this amount");
+        if (ctx.message.text?.toLowerCase() === "no") {
+          config.value.minBuy = false;
+          await configDao.updateConfig(config);
+          await ctx.reply("Okay, got it");
+          return;
+        }
+        // @ts-expect-error message.text is string | undefined
+        if (Number.isNaN(parseInt(ctx.message.text))) {
+          await ctx.reply("Please send me a valid number");
+          return;
+        }
+        // @ts-expect-error message.text is string | undefined
+        if (ctx.message.text?.length >= 1) {
+          // @ts-expect-error message.text is string | undefined
+          config.value.minBuy = ctx.message.text.toString();
+          await configDao.updateConfig(config);
+          await ctx.reply("Okay, I'll use this amount");
+        }
       }
+    } catch (err) {
+      logger.error(`Error while processing message: ${inspect(err)}`);
+      await ctx.reply("Something went wrong");
     }
   });
   telegraf.launch();
